2bae2032998521b39f384c23c3e927fbbf2affdf
Bruno Dias <contact@diasbruno.com.br>
2011-12-17 00:46:58 -0300

    added OFCreate.rb. TODO: everything

---
+++ ruby-lib/OFCreate.rb
@@ -0,0 +1,14 @@
+class OFCreate < OFBase
+  
+    ####################################################
+    # Initialize
+    ####################################################
+    def initialize()
+        super()
+    end
+    
+    def create()
+        header("oF - Creating a new project...")
+    end
+  
+end
